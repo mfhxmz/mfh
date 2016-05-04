15a69f992dfbb5ca9826cd1176728bbc02f27108
wxsm <edisond@qq.com>
2016-05-05 00:35:04 +0800

    Add Activity API

--- modules/common/server/controllers/common.server.controller.js
+++ modules/common/server/controllers/common.server.controller.js
@@ -7,12 +7,43 @@ var mysql = require('mysql');
 
 exports.queryProduct = function (req, res) {
 
+  var startFrom = req.query.startFrom || 0;
+  var limitTo = req.query.limitTo || 100;
   var type = req.query.type;
 
-  var sql = 'select tjspxx.*,spjbxx.*,spct.* from sp_tjspxx tjspxx,sp_spjbxx spjbxx, sp_spct spct where tjspxx.TCLB=? and tjspxx.SPID=spjbxx.SPID and tjspxx.SPID=spct.SPID';
-  var inserts = [type];
-  sql = mysql.format(sql, inserts);
+  if (type) {
+    var sql = 'select tjspxx.*,spjbxx.*,spct.* from sp_tjspxx tjspxx,sp_spjbxx spjbxx, sp_spct spct where tjspxx.TCLB=? and tjspxx.SPID=spjbxx.SPID and tjspxx.SPID=spct.SPID';
+    sql += ' limit ' + startFrom + ',' + limitTo;
+    var inserts = [type];
+    sql = mysql.format(sql, inserts);
 
+    console.log('[SQL]', sql);
+
+    var connection = mysql.createConnection(mysqlConfig.mysql);
+    connection.connect();
+    connection.query({
+      sql: sql,
+      nestTables: true
+    }, function (err, data) {
+      if (err) {
+        console.error(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+    connection.end();
+  } else {
+    res.sendStatus(500);
+  }
+};
+
+
+exports.queryActivity = function (req, res) {
+  var startFrom = req.query.startFrom || 0;
+  var limitTo = req.query.limitTo || 100;
+  var sql = 'select cxhdxx.*,cxhdspxx.*,cxhdgzxx.*,cxhdfxfw.* from sp_cxhdxx cxhdxx,sp_cxhdspxx cxhdspxx,sp_cxhdgzxx cxhdgzxx,sp_cxhdfxfw cxhdfxfw where cxhdxx.HDXXID=cxhdspxx.HDXXID and cxhdxx.HDXXID=cxhdgzxx.HDXXID and cxhdgzxx.HDGZID=cxhdfxfw.HDGZID';
+  sql += ' limit ' + startFrom + ',' + limitTo;
   console.log('[SQL]', sql);
 
   var connection = mysql.createConnection(mysqlConfig.mysql);
--- modules/common/server/routes/common.server.routes.js
+++ modules/common/server/routes/common.server.routes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   var common = require('../controllers/common.server.controller');
 
   app.route('/api/query/product').get(common.queryProduct);
+  app.route('/api/query/activity').get(common.queryActivity);
 };
